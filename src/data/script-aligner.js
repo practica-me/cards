6b4bc9f05bfb7a4e493cdc36ec4b43b15e0390c0
prabhasp <prabhas.pokharel@gmail.com>
2017-09-11 20:56:07 -0700

    Updated script-aligner to work as a node script, updating data.

--- src/data/script-aligner.js
+++ src/data/script-aligner.js
@@ -19,8 +19,8 @@ function destructivelyAlignScript(practicaJSON, gentleJSON) {
      convoElement.conversation = convoElement.conversation.map(function(line) {
        var lineStartOffset = transcriptTxt.indexOf(line);
        if (lineStartOffset < 0) {
-         console.log("ERROR: didn't find the following line:");
-         console.log(line);
+         console.error("ERROR: didn't find the following line:");
+         console.error(line);
          return {
            text: line,
            audioStart: undefined,
@@ -32,9 +32,9 @@ function destructivelyAlignScript(practicaJSON, gentleJSON) {
          var startWord = wordsByStart[lineStartOffset];
          var endWord = wordsByEnd[lineEndOffset];
          if (!startWord || !endWord) {
-           console.log("ERROR: start or end word not found for line:");
-           console.log(line, lineStartOffset, lineEndOffset);
-           console.log("startWord", startWord, "endWord", endWord);
+           console.error("ERROR: start or end word not found for line:");
+           console.error(line, lineStartOffset, lineEndOffset);
+           console.error("startWord", startWord, "endWord", endWord);
          }
          return {
            text: line,
@@ -51,9 +51,26 @@ function destructivelyAlignScript(practicaJSON, gentleJSON) {
          convoElement.titleAudioEnd = line.audioEnd;
        }
      });
-     console.log(JSON.stringify(practicaJSON));
-   })
-
+   });
+   return practicaJSON;
 }
 
-export default destructivelyAlignScript;
+var USAGE = "Example usage:\n" +
+  "node script-aligner.js short_replies.json short_replies_aligned.json > short_replies_annotated.json"
+if(process.argv.length < 4) {
+  console.log(USAGE);
+  console.log(process.argv);
+  throw "script-alinger requires path to XML data";
+} else {
+  var practicaJSON = require('./' + process.argv[2]);
+  var alignedJSON = require('./' + process.argv[3]);
+  console.log(
+    JSON.stringify(
+      destructivelyAlignScript(practicaJSON, alignedJSON),
+      null,
+      ' '
+    ));
+}
+
+
+//export default destructivelyAlignScript;
