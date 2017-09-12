b3cd7da3769cb574a01d5845da35527437972492
prabhasp <prabhas.pokharel@gmail.com>
2017-09-12 13:52:08 -0700

    Adding fullstory

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import ScriptViewer from './components/script-viewer.js';
 import SCRIPT_MODES from './consts.js';
 
 import script from './data/short_replies_annotated.json';
+import { FullStory } from 'react-fullstory-component';
+
+const settings = {
+    debug: false,
+    host: 'www.fullstory.com',
+    orgKey: '6GPQQ'
+};
+const data = {
+    script: 'short_replies'
+};
 
 class App extends Component {
   render() {
     return(
-      <ScriptViewer script={script} mode={SCRIPT_MODES.TITLE_AUDIO}/>
+      <div>
+        <ScriptViewer script={script} mode={SCRIPT_MODES.TITLE_AUDIO}/>
+        <FullStory settings={settings} custom={data} />
+      </div>
     );
   }
 }
-
 export default App;
