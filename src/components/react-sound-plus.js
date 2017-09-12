74f585316a455106a83930fbaeaf2c2ca25e5021
prabhasp <prabhas.pokharel@gmail.com>
2017-09-12 11:41:04 -0700

    Migrating react-sound to a local react-sound-plus

---
+++ src/components/react-sound-plus.js
@@ -0,0 +1,201 @@
+import React from 'react';
+import T from 'prop-types';
+
+const pendingCalls = [];
+let initialized = false;
+
+let soundManager;
+if (typeof window !== 'undefined') {
+  soundManager = require('soundmanager2').soundManager;
+
+  soundManager.onready(() => {
+    pendingCalls.slice().forEach(cb => cb());
+  });
+}
+
+function createSound(options, cb) {
+  if (soundManager.ok()) {
+    cb(soundManager.createSound(options));
+    return () => {};
+  } else {
+    if (!initialized) {
+      initialized = true;
+      soundManager.beginDelayedInit();
+    }
+
+    const call = () => {
+      cb(soundManager.createSound(options));
+    };
+
+    pendingCalls.push(call);
+
+    return () => {
+      pendingCalls.splice(pendingCalls.indexOf(call), 1);
+    };
+  }
+}
+
+function noop() {}
+
+const playStatuses = {
+  PLAYING: 'PLAYING',
+  STOPPED: 'STOPPED',
+  PAUSED: 'PAUSED'
+};
+
+export default class Sound extends React.Component {
+  static status = playStatuses;
+
+  static propTypes = {
+    url: T.string.isRequired,
+    playStatus: T.oneOf(Object.keys(playStatuses)).isRequired,
+    position: T.number,
+    playFromPosition: T.number,
+    volume: T.number,
+    onLoading: T.func,
+    onLoad: T.func,
+    onPlaying: T.func,
+    onPause: T.func,
+    onResume: T.func,
+    onStop: T.func,
+    onFinishedPlaying: T.func,
+    autoLoad: T.bool,
+    loop: T.bool,
+  };
+
+  static defaultProps = {
+    playFromPosition: 0,
+    volume: 100,
+    onLoading: noop,
+    onLoad: noop,
+    onPlaying: noop,
+    onPause: noop,
+    onResume: noop,
+    onStop: noop,
+    onFinishedPlaying: noop,
+    autoLoad: false,
+    loop: false,
+  };
+
+  componentDidMount() {
+    this.createSound(sound => {
+      if (this.props.playStatus === playStatuses.PLAYING) {
+        sound.play();
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeSound();
+  }
+
+  componentDidUpdate(prevProps) {
+    const withSound = (sound) => {
+      if (!sound) { return; }
+
+      if (this.props.playStatus === playStatuses.PLAYING) {
+        if (sound.playState === 0) {
+          sound.play();
+        }
+
+        if (sound.paused) {
+          sound.resume();
+        }
+      } else if (this.props.playStatus === playStatuses.STOPPED) {
+        if (sound.playState !== 0) {
+          sound.stop();
+        }
+      } else { // this.props.playStatus === playStatuses.PAUSED
+        if (!sound.paused) {
+          sound.pause();
+        }
+      }
+
+      if (this.props.playFromPosition !== prevProps.playFromPosition) {
+        sound.setPosition(this.props.playFromPosition);
+      }
+
+      if (this.props.position != null) {
+        if (sound.position !== this.props.position &&
+          Math.round(sound.position) !== Math.round(this.props.position)) {
+
+          sound.setPosition(this.props.position);
+        }
+      }
+
+      if (this.props.volume !== prevProps.volume) {
+        sound.setVolume(this.props.volume);
+      }
+    };
+
+    if (this.props.url !== prevProps.url) {
+      this.createSound(withSound);
+    } else {
+      withSound(this.sound);
+    }
+  }
+
+  createSound(callback) {
+    this.removeSound();
+
+    const props = this.props;
+    const that = this;
+
+    if (!props.url) { return; }
+
+    this.stopCreatingSound = createSound({
+      url: this.props.url,
+      autoLoad: props.autoLoad,
+      volume: props.volume,
+      position: this.props.playFromPosition || this.props.position || 0,
+      whileloading() {
+        props.onLoading(this);
+      },
+      onload() {
+        props.onLoad(this);
+      },
+      whileplaying() {
+        props.onPlaying(this);
+      },
+
+      onpause() {
+        props.onPause(this);
+      },
+      onresume() {
+        props.onResume(this);
+      },
+      onstop() {
+        props.onStop(this);
+      },
+      onfinish() {
+        if (that.props.loop && that.props.playStatus === playStatuses.PLAYING) {
+          that.sound.play()
+        } else {
+          props.onFinishedPlaying();
+        }
+      }
+    }, sound => {
+      this.sound = sound;
+      callback(sound);
+    });
+  }
+
+  removeSound() {
+    if (this.stopCreatingSound) {
+      this.stopCreatingSound();
+      delete this.stopCreatingSound;
+    }
+
+    if (this.sound) {
+      try {
+        this.sound.destruct();
+      } catch (e) {} // eslint-disable-line
+
+      delete this.sound;
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
--- src/components/sound-sprite.js
+++ src/components/sound-sprite.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Sound from 'react-sound';
+import Sound from './react-sound-plus.js';
 
 class SoundSprite extends Component {
   constructor(props) {
@@ -10,7 +10,8 @@ class SoundSprite extends Component {
       if(!(key in this.props))
         console.error("Missing required key", key, 'in props', this.props);
     });
-    this.state = {position: props.audioStart, playing: props.playing};
+    this.state = {position: props.audioStart, playing: props.playing,
+                  loaded: false, errorMsg: undefined};
     this.play = this.play.bind(this);
     this.pause = this.pause.bind(this);
   }
@@ -31,7 +32,16 @@ class SoundSprite extends Component {
     this.setState({playing: false})
   }
   render() {
+    console.log(this.state.position, this.props.audioStart);
     var _this = this;
+    var onLoad = (obj) => {
+      if (obj.readyState < 3) { // soundmanager-2: readyState = 3 is loaded state
+        this.setState({errorMsg: "Sound not loaded :( :(", loaded: false})
+      } else {
+        this.setState({loaded: true, errorMsg: ''})
+      }
+      console.log("Sound loaded!")
+    }
     var onPlaying = (obj) => {
       var {position} = obj;
       // If finished, reset position to the start of sprite, and pause playing
@@ -44,17 +54,19 @@ class SoundSprite extends Component {
         this.props.onPlaying(position);
       }
     }
-    var playOrPause = this.state.playing ?
+    var playOrPause = this.props.hidePlayPause ? "" : (this.state.playing ?
      <button className="playpause pause" onClick={this.pause}>
       {this.props.pauseText || "Pause"} </button> :
      <button className="playpause play" onClick={this.play}>
-      {this.props.playText || "Play"} </button>;
+      {this.props.playText || "Play"} </button>);
     return(
       <div className="sound-sprite">
-        {this.props.hidePlayPause ? "" : playOrPause}
+        {this.state.errorMsg}
+        {(this.state.loaded ? playOrPause : "Loading audio...")}
         <Sound url={this.props.audio_url}
                autoLoad={true}
                playStatus={this.state.playing ? Sound.status.PLAYING : Sound.status.PAUSED}
+               onLoad={onLoad}
                position={this.state.position}
                onPlaying={onPlaying} />
       </div>
