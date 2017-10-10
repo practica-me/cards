7f530a6f9f25b07e27359a0e74f421be90984488
prabhasp <prabhas.pokharel@gmail.com>
2017-10-10 16:17:43 -0700

    Adding /unit/unit-name based routing to the app.

--- src/components/all-units-viewer.js
+++ src/components/all-units-viewer.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import UnitViewer from './unit-viewer.js';
 import T from 'prop-types';
 import './all-units-viewer.css';
+import { Route, Redirect } from 'react-router-dom'
+
+const DEFAULT_UNIT_PATH = '/unit/casual-interactions/';
 
 export default class AllUnitsViewer extends Component {
+  constructor(props) {
+    super(props);
+    this.renderUnitFromMatch = this.renderUnitFromMatch.bind(this);
+  }
   static propTypes = {
     units: T.array.isRequired
   }
+  renderUnitFromMatch ({ match }) {
+    var normalize = (s) => s && s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+    var unit = this.props.units.find(function(unit) {
+      return normalize(unit.name) === normalize(match.params.name);
+    });
+    if (unit) {
+      return <UnitViewer unit={unit} />
+    } else {
+      return <RedirectToDefaultUnit />
+    }
+  }
   render() {
-    /* For now, just render the first unit. */
     return(
       <div className="all-units">
-        <UnitViewer unit={this.props.units[0]} />
+        <Route path="/" component={RedirectToDefaultUnit} exact={true} />
+        <Route path="/unit/:name" render={this.renderUnitFromMatch} />
       </div>
     );
   }
 }
+
+const RedirectToDefaultUnit = ({match}) => (
+  <Redirect to={{pathname: DEFAULT_UNIT_PATH}} />
+);
