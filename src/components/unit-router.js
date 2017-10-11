30f8e6201bd79f8e7bbc25a6113ad430bf73abbd
prabhasp <prabhas.pokharel@gmail.com>
2017-10-11 09:04:46 -0700

    Adding lesson routing.

---
+++ src/components/unit-router.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import UnitViewer from './unit-viewer.js';
+import T from 'prop-types';
+
+import { Route, Redirect } from 'react-router-dom'
+
+const DEFAULT_UNIT = 'casual-interactions';
+const DEFAULT_LESSON = '1';
+
+const unitLessonPath = (optionalUnitName, optionalLessonNumber) => ( '/unit/' + (optionalUnitName || DEFAULT_UNIT) + '/lesson/' + (optionalLessonNumber || DEFAULT_LESSON));
+
+const RedirectToDefaultPath = ({match}) => (
+  <Redirect to={{pathname: unitLessonPath()}} />
+);
+
+const normalizeString = (s) => (
+  s && s.toLowerCase().replace(/[^a-z0-9]/gi,'')
+);
+
+export default class UnitRouter extends Component {
+  constructor(props) {
+    super(props);
+    this.renderUnitFromMatch = this.renderUnitFromMatch.bind(this);
+  }
+  static propTypes = {
+    units: T.array.isRequired
+  }
+  renderUnitFromMatch ({ match }) {
+    var unit = this.props.units.find(function(unit) {
+      return normalizeString(unit.name) === normalizeString(match.params.unit_name);
+    });
+    var lNum = parseInt(match.params.lesson_number, 10);
+    /* lessonNumber 1-indexed */
+    var lessonNumber = (isNaN(lNum) || lNum < 0 || 
+                     lNum > unit.lessons.length) ?
+        undefined : (lNum);
+    if (unit && lessonNumber) {
+      return <UnitViewer unit={unit} 
+                         lessonIndex={lessonNumber - 1} />
+    } else {
+      var path = unitLessonPath(unit && unit.name);
+      return <Redirect to={{pathname: path}} />;
+    }
+  }
+  render() {
+    return(
+      <div className="all-units">
+        <Route path="/" component={RedirectToDefaultPath} exact={true} />
+        <Route path="/unit/:unit_name/lesson/:lesson_number" render={this.renderUnitFromMatch} />
+      </div>
+    );
+  }
+}
--- src/components/unit-viewer.js
+++ src/components/unit-viewer.js
@@ -1,38 +1,49 @@
 import React, { Component } from 'react';
 import T from 'prop-types';
 import {Icon} from 'react-fa';
+import './unit-viewer.css';
+
+import {Link} from 'react-router-dom';
 
 import LessonViewer from './lesson-viewer.js';
 
 export default class UnitViewer extends Component {
   constructor(props) {
     super(props);
-    this.state = {lessonIndex: 0, lessonStarted: false};
+    this.state = {lessonStarted: false};
     this.renderArrow = this.renderArrow.bind(this);
   }
   static propTypes = {
     unit: T.object.isRequired,
   }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.lessonIndex !== this.props.lessonIndex) {
+      this.setState({lessonStarted: false});
+    }
+  }
   // by default, we go Forward, unless, of course, goBackward is true
   renderArrow(goBackward) {
     var allLessons = this.props.unit.lessons;
-    var lessonIndex = this.state.lessonIndex;
+    var lessonIndex = this.props.lessonIndex;
     var nextIndex = goBackward ? (lessonIndex - 1) : (lessonIndex + 1);
     var vizClass = "";
     if (nextIndex < 0 || nextIndex >= allLessons.length) {
+      // we don't want to render it but make the layout easy
       nextIndex = lessonIndex;
-      vizClass = "transparent"; // we don't want to render it but make the layout easy
+      vizClass = "transparent"; 
     }
     var dirClass = goBackward ? "left" : "right";
     var nextLesson = allLessons[nextIndex];
     var icon = goBackward ? "chevron-left" : "chevron-right";
-    var changeLesson = () => this.setState(
-        {lessonIndex: nextIndex, lessonStarted: false});
+    /* lessonNumber 1-indexed */
+    var pathFromIdx = (lIdx) => (
+      "/unit/" + this.props.unit.name + "/lesson/" + (lIdx + 1)
+    );
     return(
       <div className={"arrow-block " + vizClass + " " + dirClass}>
-        <button className="minimal arrow" onClick={changeLesson} >
+        <Link to={{pathname: pathFromIdx(nextIndex)}} >
           <Icon name={icon} />
-        </button>
+        </Link>
         <div className="next-lesson hidden-on-mobile hidden-on-phablet">
           Lesson {nextLesson.number}: <br/>
           {nextLesson.name}
@@ -42,7 +53,8 @@ export default class UnitViewer extends Component {
   }
   render() {
     var unit = this.props.unit;
-    var lesson = unit.lessons[this.state.lessonIndex];
+    var lessonIndex = this.props.lessonIndex || 0;
+    var lesson = unit.lessons[lessonIndex];
     return(
         <div className="unit">
           {this.state.lessonStarted ? "" : this.renderArrow(true)}
