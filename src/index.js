622725a9bd342190f42497a9ae57391fe6508b0c
prabhasp <prabhas.pokharel@gmail.com>
2017-10-10 21:40:33 -0700

    BrowserRouter -> HashRouter

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render((
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   ), document.getElementById('root'));
 registerServiceWorker();
